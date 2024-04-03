60d883723a2bd1cab269e1d279dcb75680aea034
hjkisgoood <hjkisgoood@bupt.edu.cn>
2024-04-03 09:38:06 +0800

    second push

--- Solution.java
+++ Solution.java
@@ -17,7 +17,7 @@ public class Solution {
     }//二分查找,力扣35题插入排序
 
     public int[] searchRange(int[] nums, int target) {
-        int left = getleftBorder(nums,target);
+        int left = getLeftBorder(nums,target);
         int right = getRightBorder(nums,target);
         int[] result = new int [2];
         if(right == -2 || left == -2) {
@@ -32,7 +32,7 @@ public class Solution {
         return new int[]{-1, -1};
 
 
-    }
+    }//力扣34题在排序数组中查找元素的第一个和最后一个位置
 
     private int getRightBorder(int nums[], int target){
         int left = 0;
@@ -49,7 +49,7 @@ public class Solution {
         }
         return rightBorder;
     }
-    private int getleftBorder(int nums[], int target){
+    private int getLeftBorder(int nums[], int target){
         int left = 0;
         int right = nums.length -1;
         int leftBorder = -2;
@@ -64,6 +64,33 @@ public class Solution {
         }
         return  leftBorder;
     }
+
+    public int mySqrt(int x) {
+        long left = 1;
+        long right = x;
+        while(left <= right){
+            long mid = left + (right - left)/2;
+            if(mid * mid > x) {
+                right = mid - 1;
+            }else left = mid + 1;
+        }
+        return (int)right;
+
+    }//69力扣,x的平方根
+
+    public boolean isPerfectSquare(int num) {
+        long right = num;
+        long left = 0;
+        while(left <= right){
+            long mid = (left + right)/2;
+            if(mid * mid == num) return true;
+            else if (mid * mid < num) {
+                left = mid + 1;
+            }else right = mid - 1;
+
+        }
+        return false;
+    }//367有效的完全平方数
 
 
     }
