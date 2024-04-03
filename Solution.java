1eaf9f6635da01d8e98cc10a4f8dc37a808dcd8f
hjkisgoood <hjkisgoood@bupt.edu.cn>
2024-04-03 11:46:40 +0800

    second push

--- Solution.java
+++ Solution.java
@@ -1,6 +1,7 @@
 package bupt;
 
 public class Solution {
+    //二分查找
     public int searchInsert(int[] nums, int target) {
         int n = nums.length;
         int left = 0,right = n-1;
@@ -91,6 +92,93 @@ public class Solution {
         }
         return false;
     }//367有效的完全平方数
+//双指针
+    public int removeDuplicates(int[] nums) {
+        int j = 1;
+        for(int i =1; i< nums.length;i++){
+            if(nums[i] != nums[i-1]){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }//26删除重复元素
+    public void moveZeroes(int[] nums) {
+        for(int i = 0,j=0;i < nums.length;i++){
+            if(nums[i] == 0) continue;
+            else {
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                j++;
+            }
+        }
+
+    }//283移动0
+
+    public boolean backspaceCompare(String s, String t) {
+        StringBuilder sBack = new StringBuilder();
+        StringBuilder tBack = new StringBuilder();
+        for(int i = 0,j=0;i < s.length();i++){
+            char c = s.charAt(i);
+            if(c != '#') {
+                sBack.append(c);
+                j++;
+            } else {
+                if(j > 0){
+                    sBack.delete(j-1,j);
+                    j--;
+                }
+            }
+        }
+        for(int i = 0,j=0;i < t.length();i++){
+            char c = t.charAt(i);
+            if(c != '#') {
+                tBack.append(c);
+                j++;
+            } else {
+                if(j > 0){
+                    tBack.delete(j-1,j);
+                    j--;
+                }
+            }
+        }
+        return sBack.toString().equals(tBack.toString());
+
+
+    }//844比较含退格的字符串
+    public int[] sortedSquares(int[] nums) {
+        int mid = 0;
+        int minSqu = Integer.MAX_VALUE;
+        for(int i = 0;i<nums.length;i++){
+            nums[i] = nums[i] * nums[i];
+            if(nums[i] < minSqu){
+                mid = i;
+                minSqu = nums[i];
+            }
+        }
+        int [] newNums = new int[nums.length];
+        int left = mid-1,right = mid+1;
+        newNums[0] =nums[mid];
+        int index = 1;
+        while(right < nums.length || left >= 0){
+            if(left >= 0 &&right < nums.length && nums[left] <= nums[right]){
+                newNums[index] = nums[left];
+                left--;
+                index++;
+            } else if (right >=nums.length) {
+                newNums[index] = nums[left];
+                left--;
+                index++;
+            } else if(right < nums.length){
+                newNums[index] = nums[right];
+                right++;
+                index++;
+            }
+        }
+
+        return newNums;
+    }//977有序数组的平方
 
 
     }
