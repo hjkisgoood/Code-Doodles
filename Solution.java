116b2b5c8fba3fb13ee9be701925cea327eb62fb
hjkisgoood <hjkisgoood@bupt.edu.cn>
2024-04-08 13:23:50 +0800

    4.8号每日打卡,链表相关链表交点

--- Solution.java
+++ Solution.java
@@ -443,6 +443,50 @@ public class Solution {
         return dummy.next;
 
     }//19删除倒数n节点
+/*  4.7号每日打卡*/
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode curA = headA;
+        ListNode curB = headB;
+        int lenA = 0,lenB = 0;
+        while(curA != null){
+            lenA++;
+            curA = curA.next;
+        }
+        while (curB != null){
+            lenB++;
+            curB = curB.next;
+        }
+        curA = headA;
+        curB = headB;
+        // 让curA为最长链表的头，lenA为其长度
+        if(lenB > lenA){
+            //1. swap (lenA, lenB);
+            int tmplen = lenA;
+            lenA = lenB;
+            lenB = tmplen;
+            //2. swap (curA, curB);
+            ListNode tmNode = curA;
+            curA = curB;
+            curB = tmNode;
+        }
+        // 求长度差
+        int gap = lenA - lenB;
+        // 让curA和curB在同一起点上（末尾位置对齐）
+        while (gap-- > 0) {
+            curA = curA.next;
+        }
+        // 遍历curA 和 curB，遇到相同则直接返回
+        while (curA != null) {
+            if (curA == curB) {
+                return curA;
+            }
+            curA = curA.next;
+            curB = curB.next;
+        }
+        return null;
+
+    }//面试题0207链表相交
 
 
     }
