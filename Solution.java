febd4c2993c8ccbbdcaebc07f249aa6a1c113b83
hjkisgoood <hjkisgoood@bupt.edu.cn>
2024-04-09 11:23:20 +0800

    4.9号每日打卡,链表相关环形链表

--- Solution.java
+++ Solution.java
@@ -487,7 +487,26 @@ public class Solution {
         return null;
 
     }//面试题0207链表相交
-    //4.8
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                ListNode index1 = fast;
+                ListNode index2 = head;
+                while(index2 != index1){
+                    index2 = index2.next;
+                    index1 = index1.next;
 
+                }
+                return index1;
+            }
+        }
+        return null;
+    }//142环形链表
+
+    
 
     }
