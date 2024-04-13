c9ddd691414f1607ce0e8d7773524d58f66fe767
贺建坤 <114862355+hjkisgoood@users.noreply.github.com>
2024-04-13 19:09:28 +0800

    4-13每日打卡关键在于坚持

--- Solution.java
+++ Solution.java
@@ -721,4 +721,46 @@ public class Solution {
         }
      }
 
+    
+    public int strStr(String haystack, String needle) {
+        if(needle.length() == 0) return 0;
+
+        int[] next = new int[needle.length()];
+        getNext(next, needle);
+        int j = -1;
+        for(int i = 0;i < haystack.length(); i++){
+            while (j >= 0 && haystack.charAt(i) != needle.charAt(j + 1)) j= next[j];
+
+            if(haystack.charAt(i) == needle.charAt(j+1)) j++;
+
+            if(j == needle.length() - 1) return (i - needle.length() + 1);
+        }
+        return -1;
+
+     }//28找出字符串中第一个匹配项的下标
+    private void getNext(int [] next,String s){
+        int j = -1;
+        next[0] = j;
+        for(int i = 1; i < s.length();i++){
+            while(j >= 0 && s.charAt(i) != s.charAt(j + 1)) j = next[j];
+
+            if(s.charAt(i) == s.charAt(j + 1)) j++;
+
+            next[i] = j;
+        }
+    }//创建next数组
+    /*
+    public int strStr(String haystack, String needle) {
+
+        int len = needle.length();
+        for(int i =0;i <= haystack.length()-len;i++){
+            String str = haystack.substring(i,i+len);
+            if(str.equals(needle)) return i;
+        }
+        return -1;
+    }
+
+     *///28找出第一个匹配项的下标 库函数版本
+
+
     }
