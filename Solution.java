f3669f07b888e73181596f37a649d9b3251972e0
贺建坤 <114862355+hjkisgoood@users.noreply.github.com>
2024-04-15 17:01:29 +0800

    4.15每日打卡

--- Solution.java
+++ Solution.java
@@ -761,6 +761,29 @@ public class Solution {
     }
 
      *///28找出第一个匹配项的下标 库函数版本
+        public boolean repeatedSubstringPattern(String s) {
+        if(s.length() == 1) return false;
+
+        int len = s.length();
+        s = " " + s;
+        char[] chars = s.toCharArray();
+        int[] next = new int[len+1];
+        // 构造 next 数组过程，j从0开始(空格)，i从2开始
+        for(int i = 2,j = 0;i <= len;i++){
+            // 匹配不成功，j回到前一位置 next 数组所对应的值
+            while (j > 0 && chars[i] != chars[j+1]) j = next[j];
+            //匹配成功
+            if( chars[i] == chars[j+1]) j++;
+            // 更新 next 数组的值
+            next[i] = j;
+        }
+        // 最后判断是否是重复的子字符串，这里 next[len] 即代表next数组末尾的值
+        if(next[len] > 0 && len % (len - next[len]) == 0) return true;
+        return false;
+
+
+    }//459重复的子字符串
+
 
 
     }
