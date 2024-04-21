2767d0301a891e4fe540a572417aae14c8cec7b3
贺建坤 <114862355+hjkisgoood@users.noreply.github.com>
2024-04-21 11:54:10 +0800

    二叉树数据结构

---
+++ TreeNode.java
@@ -0,0 +1,16 @@
+package bupt;
+
+import sun.reflect.generics.tree.Tree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(){};
+    TreeNode(int val){this.val = val;}
+    TreeNode(int val, TreeNode left, TreeNode right){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}//二叉树
