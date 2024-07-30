e52e68a59b0a6eda06dc7ecfd9c3a6e3ef508e31
贺建坤 <114862355+hjkisgoood@users.noreply.github.com>
2024-07-30 15:01:33 +0800

    7-30每日打卡，动态规划贪心算法

--- Solution2.java
+++ Solution2.java
@@ -2,9 +2,9 @@ package org.example;
 
 
 import java.net.StandardSocketOptions;
-import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.*;
 
+import static java.util.Arrays.binarySearch;
 import static java.util.Arrays.sort;
 
 public class Solution2 {
@@ -236,15 +236,134 @@ public class Solution2 {
 
     }//452.用最少的箭射爆气球
 
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) ->{
+            if(a[1] != b[1]){
+                return Integer.compare(a[1], b[1]);
+            }else {
+                return Integer.compare(b[0], a[0]);
+            }
+        });
+        System.out.println(Arrays.deepToString(intervals));
+
+        int count = 1;
+        for(int i = 1; i < intervals.length; i++){
+            if(intervals[i][0] < intervals[i - 1][1]){
+                intervals[i][1] = Math.min(intervals[i - 1][1], intervals[i][1]);
+            }else {
+                count++;
+            }
+        }
+        return intervals.length - count;
+
+    }//435.无重叠区间
+
+    public List<Integer> partitionLabels(String s) {
+        List<Integer> res = new LinkedList<>();
+        int []edge = new int[26];
+        char[] chars = s.toCharArray();
+        for(int i = 0; i< chars.length; i++){
+            edge[chars[i] - 'a'] = i;
+        }
+        int idx = 0;
+        int last = -1;
+        for (int i = 0;i< chars.length; i++){
+            idx = Math.max(idx, edge[chars[i] - 'a']);
+            if(i == idx){
+                res.add(i - last);
+                last = i;
+            }
+        }
+        return res;
+
+
+    }//763.划分字母边界
+
+    public int[][] merge(int[][] intervals) {
+        List<int[]> res = new ArrayList<>();
+        Arrays.sort(intervals, Comparator.comparingInt(x -> x[0]));
+        int start = intervals[0][0];
+        int rightBound = intervals[0][1];
+        for(int i = 1; i < intervals.length; i++){
+            //左边界大于右边界
+            //找到不重叠区间，更新左右边界
+            if(intervals[i][0] > rightBound){
+                res.add(new int[]{start, rightBound});
+                start = intervals[i][0];
+                rightBound = intervals[i][1];
+            }else {
+                //重叠区间，更新右边界
+                rightBound = Math.max(rightBound, intervals[i][1]);
+            }
+        }
+        //最后一个区间没有处理，这里添加上
+        res.add(new int[]{start, rightBound});
+        System.out.println(Arrays.deepToString(res.toArray(new int[res.size()][])));
+        return res.toArray(new int[res.size()][]);
+
+
+    }//56.合并区间
+
+    public int monotoneIncreasingDigits(int n) {
+        String s = String.valueOf(n);
+        char[] chars = s.toCharArray();
+        int start = s.length();
+        for(int i = s.length() - 2;i >= 0; i--){
+            if(chars[i] > chars[i + 1]){
+                chars[i]--;
+                start = i + 1;
+            }
+        }
+        for(int i = start;i < s.length(); i++){
+            chars[i] = '9';
+        }
+        int res = 0;
+        for (char aChar : chars) {
+            res = res * 10 + aChar - '0';
+        }
+        return res;
+    }//738.单调递增的数字
 
 
 
+//动态规划
+    public int fib(int n) {
+        if(n == 0){
+            return 0;
+        }
+        int [] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2;i < n+1; i++){
+            dp[i] = dp[i-2] + dp[i-1];
+        }
+        return dp[n];
+    }//509.斐波那契
 
+    public int climbStairs(int n) {
+        if(n == 1){
+            return 1;
+        }
+        int [] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        for(int i = 2;i < n; i++){
+            dp[i] = dp[i-2] + dp[i-1];
+        }
+        return dp[n-1];
+    }//70.爬楼梯
 
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int [] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 0;
+        for(int i = 2;i < n+1; i++){
+            dp[i] = Math.min(dp[i-2] + cost[i-2], dp[i-1] + cost[i-1]);
+        }
+        return dp[n];
 
-
-
-
+    }//746.使用最小话费爬楼梯
 
 
 
